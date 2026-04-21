4c89aac9e35dc86ccd4114f62db5b037f251f79e
Jaime Alvarez Diaz <alvarez.diaz.jaime1@gmail.com>
2026-04-21 10:27:07 +0200

    Add xywhr2xyxyxyxy to utils.boxes

--- vimi/utils/boxes.py
+++ vimi/utils/boxes.py
@@ -25,7 +25,31 @@ from shapely.geometry import Polygon
 from ..logs import vimi_logger
 
 
-def poly_from_xyxyxyxy(box: np.ndarray) -> Polygon:
+def xywhr2xyxyxyxy(xywhr: np.ndarray) -> np.ndarray:
+    """Get the coordinates of the corners of an array of rotated boxes.
+    Args:
+        xywhr (np.ndarray): n x [xc, yc, w, h, rad] array with rotated boxes representation.
+    Returns:
+        np.ndarray: n x [x1, y1, x2, y2, x3, y3, x4, y4] with the corners coords of the box.
+    """
+    ctr: np.ndarray = xywhr[..., :2]
+    w: float
+    h: float
+    angle: float
+    w, h, angle = (float(xywhr[..., i : i + 1]) for i in range(2, 5))
+    cos_value: float = np.cos(angle)
+    sin_value: float = np.sin(angle)
+    vec1: list[float] = [w / 2 * cos_value, w / 2 * sin_value]
+    vec2: list[float] = [-h / 2 * sin_value, h / 2 * cos_value]
+    vec1_array: np.ndarray = np.concatenate(vec1, -1)
+    vec2_array: np.ndarray = np.concatenate(vec2, -1)
+    pt1 = ctr + vec1_array + vec2
+    pt2 = ctr + vec1_array - vec2_array
+    pt3 = ctr - vec1_array - vec2_array
+    pt4 = ctr - vec1_array + vec2_array
+    return np.stack([pt1, pt2, pt3, pt4], -2)
+
+def xyxyxyxy2poly(box: np.ndarray) -> Polygon:
     if box.shape != (8,):
         msg: str = f'box shape must be (8,), but got {box.shape}.'
         vimi_logger.error(msg)
